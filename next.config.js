26f6e40f6050335a588de45be2265adc689b3c19
HSevenn <91921247+HSevenn@users.noreply.github.com>
2025-09-03 23:13:00 -0500

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { appDir: true },
-  images: { unoptimized: true },
-}
-module.exports = nextConfig
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
