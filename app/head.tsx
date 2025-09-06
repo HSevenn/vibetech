7ba841fde6af7a9bafbdd95139026a1e2a61a083
HSevenn <91921247+HSevenn@users.noreply.github.com>
2025-09-06 17:26:11 -0500

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/vibetech_icon.ico" />
+      <link rel="apple-touch-icon" href="/vibetech_icon_180.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/vibetech_icon_192.png" />
+      <link rel="icon" type="image/png" sizes="512x512" href="/vibetech_icon_512.png" />
+    </>
+  );
+}
