ddfbc83ce60a3666f06456cee718b1573ada0f11
HSevenn <91921247+HSevenn@users.noreply.github.com>
2025-09-06 17:32:40 -0500

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,11 @@
 export default function Head() {
   return (
     <>
-      <link rel="icon" href="/vibetech_icon.ico" />
+      <link rel="icon" href="/vibetech_icon.ico" sizes="any" />
       <link rel="apple-touch-icon" href="/vibetech_icon_180.png" />
+      <link rel="icon" type="image/png" sizes="16x16"  href="/vibetech_icon_16.png" />
+      <link rel="icon" type="image/png" sizes="32x32"  href="/vibetech_icon_32.png" />
+      <link rel="icon" type="image/png" sizes="48x48"  href="/vibetech_icon_48.png" />
       <link rel="icon" type="image/png" sizes="192x192" href="/vibetech_icon_192.png" />
       <link rel="icon" type="image/png" sizes="512x512" href="/vibetech_icon_512.png" />
     </>
